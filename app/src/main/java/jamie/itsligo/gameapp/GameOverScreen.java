1b73c6eaa297f5428dbc87b2f2489dc2433b031b
Jamie Donohoe <S00189802@mail.itsligo.ie>
2020-12-14 16:37:17 +0000

    first commit

---
+++ app/src/main/java/jamie/itsligo/gameapp/GameOverScreen.java
@@ -0,0 +1,14 @@
+package jamie.itsligo.gameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameOverScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over_screen);
+    }
+}
---
+++ app/src/main/java/jamie/itsligo/gameapp/HiScoreScreen.java
@@ -0,0 +1,14 @@
+package jamie.itsligo.gameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HiScoreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hi_score_screen);
+    }
+}
